f804b6bf633a934cfa8b07881acae204e65236ba
gabycode <86427446+gabycode@users.noreply.github.com>
2023-04-17 12:43:25 -0400

    add: poems.js

---
+++ src/poems/poems.js
@@ -0,0 +1,514 @@
+export const poems = [
+  {
+    id: "I",
+    title: "I",
+    text: `<p class="verse" tabindex="0">Al picar la sexta cuerda de mi guitarra en el quinto traste,<br>
+Si está bien afinada, la quinta cuerda vibrará también.<p>
+
+<p class="verse" tabindex="0">Aun siendo distintas verdades y diferentes matices,<br>
+Hasta el silencio, se han de hablar como almas gemelas.</p>
+
+<p class="verse" tabindex="0">Igual nosotros al ser entendidos.<br>
+No ha de confundirse con alguna emoción,</p>
+
+<p class="verse" tabindex="0">Más bien, que hablamos el mismo idioma místico.<br>
+De aquí… Sale el fuego.</p>
+`,
+  },
+  {
+    id: "II",
+    title: "II",
+    text: `<p class="verse" tabindex="0">La verdad que hay veces, que no puedo hacer nada; pero no fracasé.<br>
+La verdad que hay veces, que no puedo decir nada; y todos me oyen.<br>
+La verdad que si te eres amigo, familiar o compañero; te quiero mucho.</p>
+
+<p class="verse" tabindex="0">La verdad es que si bien las cosas pasan, y me duelen; soy muy feliz.</p>
+`,
+  },
+  {
+    id: "III",
+    title: "III - Entre el fuego y la tierra",
+    text: `<p class="verse" tabindex="0">¿Cuántas mil y una noches habrás contado en tabernas?
+
+<p class="verse" tabindex="0">Entre la tierra y el fuego mueren para renacer los árboles que cantan.<br>
+Cada lágrima nace solo más fuerte que la anterior.<br>
+Pero es el corazón que llora.<br>
+Nuestros ojos los que palpitan al ritmo del desastre que ven.</p>
+
+<p class="verse" tabindex="0">¿Cuántas mil y una noches contaré antes de besarte?</p>
+
+<p class="verse" tabindex="0">Donde me dejarás ser tuyo?<br>
+Por qué preguntar cuando, nunca me dirias... pero si o no...<br>
+Te escapas, porque juegas con mi pasión.<br>
+Que mi tempestad nunca se acabe.</p>
+
+<p class="verse" tabindex="0">Te encontraré carajo.</p>
+
+<p class="verse" tabindex="0">Mil y una noches después.</p>
+
+          </p>`,
+  },
+  {
+    id: "IV",
+    title: "IV - Analysing… Error 42",
+    text: `<p class="verse" tabindex="0">Philosophical mode... Engage<br>
+Systems evaluation... Check<br>
+Hardware evaluation... Check<br>
+Brain temperature levels... Stable<br>
+Coffee intake... 110%<br>
+Software boot... Initiate<br>
+*insert boot jingle*<br>
+Archive analysis... Initiate<br>
+File compilation... Complete<br>
+System functions... Online<br>
+*Good morning Rick. How may I be of service today?*<br>
+"What is the meaning of life?"<br>
+Analysing... Error42<br>
+System status... Overloading<br>
+Overload safety protocol... System reset<br>
+System reset... Complete<br>
+*Good morning Rick. How may I be of service today?*<br>
+"What is the mea... Fuck it. Open TikTok."</p>
+`,
+  },
+  {
+    id: "V",
+    title: "V - La Cicatriz en su pierna",
+    text: `<p class="verse" tabindex="0">Es de admirar<br>
+Las imperfecciones del cuerpo<br>
+La separa de las demás<br>
+Y me enamoro de nuevo</p>
+
+<p class="verse" tabindex="0">Caricias y besos<br>
+La conmueven a suspirar<br>
+Mientras mis dedos<br>
+Rejuegan con su marca</p>
+
+<p class="verse" tabindex="0">Como la obtuvo es de olvido continuo<br>
+Ya no es marca de dolor<br>
+Es espacio para nosotros<br>
+Para tocar</p>
+
+<p class="verse" tabindex="0">Ella es mía<br>
+Y yo le pertenezco</p>
+`,
+  },
+  {
+    id: "VI",
+    title: "VI - Divine Elegance",
+    text: `<p class="verse" tabindex="0">Words and messages drift upon your wings<br>
+Lightly and heavenly as a breath of God himself<br>
+Taken to peace and a world of high hopes<br>
+I can't imagine my <strong>life</strong> without your visits</p>
+
+<p class="verse" tabindex="0">Forgetful minds appreciate memories<br>
+Moments stop... <strong>Breath</strong>, remember and exhale<br>
+Perhaps a blank state of mind would suit to define you<br>
+Yet, if but a thought opened, you'd go on...</p>
+
+<p class="verse" tabindex="0"><em>... I'll forget tomorrow.</em></p>
+
+<p class="verse" tabindex="0">Imagine her dark hair, big beautiful <strong>eyes</strong><br>
+Held together and eternal with your blanket<br>
+My regret must be to leave the park<br>
+And the whispers of the trees...</p>
+
+<p class="verse" tabindex="0"><em>... we'll  talk tomorrow, I hope.</em></p>
+`,
+  },
+  {
+    id: "VII",
+    title: "VII (Permanent Damage)",
+    text: `<p class="verse" tabindex="0">"When the world is quiet,<br>
+I can still hear noise.<br>
+And it won't let me sleep."</p>
+`,
+  },
+  {
+    id: "VIII",
+    title: "VIII",
+    text: `<p class="verse" tabindex="0">Hoy, ahora mismo: es todo.</p>
+
+<p class="verse" tabindex="0">La anciana de vestido rojo,<br>
+sin falta en la iglesia.</p>
+
+<p class="verse" tabindex="0">La señora que se detuvo<br>
+A orarle a San Miguel.</p>
+
+<p class="verse" tabindex="0">El hombre que llevaba comida.</p>
+
+<p class="verse" tabindex="0">El café en mí.</p>
+
+<p class="verse" tabindex="0">La vida, está aquí para quedarse.<br>
+Y eso es bueno.</p>
+
+<p class="verse" tabindex="0">Hoy, ahora mismo:<br>
+Me gusta mucho.</p>
+`,
+  },
+  {
+    id: "IX",
+    title: "IX",
+    text: `<p class="verse" tabindex="0">¿Alguna vez habían sido perturbados tus latidos<br>
+Por el vibrato de un alma?</p>
+
+<p class="verse" tabindex="0">Le daría mis manos por su cante.<br>
+Le daría mi voz por sus manos.</p>
+
+<p class="verse" tabindex="0">Pero no tengo valor para su compañía.<br>
+Obvio, si soy esclavo de esta maldita bendición.</p>
+
+<p class="verse" tabindex="0">Aún no encuentro deshacer el hechizo.<br>
+Quizás merezca caer prisionero.<br>
+Que por egoísta quedo fuera.</p>
+
+<p class="verse" tabindex="0">Este es el verso que introduzco<br>
+Solo por estética.<br>
+Por durar con cuidado<br>
+Lo completamente necesario.</p>
+
+<p class="verse" tabindex="0">Todavía llovizna por allá lejos.<br>
+En tierra.<br>
+Tierra de montañas.</p>
+`,
+  },
+  {
+    id: "X",
+    title: "X",
+    text: `<p class="verse" tabindex="0">Café de mi tierra,<br>
+Calentao con fuego de corazón.</p>
+
+<p class="verse" tabindex="0">Miro por mirar,<br>
+Pero aquí es que quiero estar.</p>
+
+<p class="verse" tabindex="0">Sentado no más bajo sol,<br>
+Hasta la noche mágica.</p>
+
+<p class="verse" tabindex="0">Que la piel se me quita,<br>
+Y er duende salga a joder.</p>
+`,
+  },
+  {
+    id: "XI",
+    title: "XI - Despacio",
+    text: `<p class="verse">Mírame<br>
+Por donde va<br>
+El corazón<br>
+Solo</p>
+
+<p class="verse">Caminar<br>
+Se hace<br>
+Más difícil<br>
+Si no estás</p>
+
+<p class="verse">Y si al correr<br>
+Pierdo la intención<br>
+Recuérdame</p>
+
+<p class="verse">Otra vez<br>
+La mente fue más<br>
+Que la emoción<br>
+Más que yo</p>
+
+<p class="verse">Me olvidé<br>
+De no confiar en nada<br>
+Menos la mejor<br>
+Versión de tí</p>
+
+<p class="verse">Y si al correr<br>
+Pierdo la intención<br>
+Recuérdame</p>
+`,
+  },
+  {
+    id: "XII",
+    title: "XII",
+    text: `<p class="verse">Me pierdo a veces<br>
+En las lunas de tu cara<br>
+En estos momentos<br>
+Que sobran las palabras</p>
+
+<p class="verse">Me refugio en tu cabello<br>
+Cárcel asegurada<br>
+El aroma de las flores<br>
+Me dicen: No pasa nada</p>
+
+<p class="verse">Esta mirada que te habla<br>
+Confiesa mi secreto<br>
+¿Cuántas veces te diré, sin hablar:<br>
+Que te quiero?</p>
+
+<p class="verse">La dulzura de tus manos,<br>
+La tendencia de tus pies.<br>
+Nos llevan a alejarnos.<br>
+Filosofía al revés.</p>
+
+<p class="verse">Un par de palabras bobas,<br>
+Como hojas con rocío.<br>
+Abrazamos fuerte el alma,<br>
+Para aguantar bien el frío.</p>
+
+<p class="verse">Esta mirada que te habla<br>
+Confiesa mi secreto<br>
+¿Cuántas veces te diré, sin hablar:<br>
+Que te quiero?</p>
+
+<p class="verse">No hay más de que hablar,<br>
+entre nosotros ya está dicho.<br>
+Encuentra tu coraje,<br>
+Y sal conmigo.</p>
+
+<p class="verse">Estas miradas que nos hablan,<br>
+Nos dicen: Ya está bueno.<br>
+Acércate un poquito más,<br>
+Y toma, todos mis besos.</p>
+`,
+  },
+  {
+    id: "XIII",
+    title: "XIII",
+    text: `<p class="verse">Llueve, y el petrichor alimenta la chimenea de mi nostalgia.</p>
+
+<p class="verse">¿Será que estoy feliz? ¿O que feliz, está muy yo hoy?</p>
+
+<p class="verse">Se lo propondré a las copas de caoba.</p>
+
+<p class="verse">Va sentado hasta que me digan.</p>
+`,
+  },
+  {
+    id: "XIV",
+    title: "XIV",
+    text: `<p class="verse">Alza la cabeza y anda tu camino,<br>
+Sin orgullo, mas con coraje.<br>
+Queda más por venir.<br>
+Pues, este amanecer,<br>
+Ni fue primero,<br>
+Ni será la última<br>
+Bendición de hoy.</p>
+
+<p class="verse">Que le demos gracias a las cosas hermosas de nuestras vidas.</p>
+
+    `,
+  },
+  {
+    id: "XV",
+    title: "XV",
+    text: `<p class="verse">A las niñas les gustan los chicos malos.<br>
+A la mujer, <strong>un</strong> hombre valiente.<br>
+Y si sabe llorar,</p>
+
+<p class="verse">Por él, ella se muere.</p>
+`,
+  },
+  {
+    id: "XVI",
+    title: "XVI",
+    text: `<p class="verse">Como todo lo natural,<br>
+Empieza joven y confusa<br>
+Disfrutando lo sencillo y esencial.</p>
+
+<p class="verse">Creciendo entenderá, <br>
+Mientras más aprende, menos sabe,<br>
+Sentirse bien, lo único que vale.</p>
+
+<p class="verse">Andando vivirá,<br>
+Curiosidad entusiasma fuerza<br>
+Empujada a conocerse<br>
+Tocando sentidos atrevidos.</p>
+
+<p class="verse">Educada y madura <br>
+Navega la fascinación del mundo<br>
+Y encanta los desaprevenidos <br>
+  pura sensualidad.</p>
+
+<p class="verse">Domarla es perder su mirada.<br>
+Controlar quitarle la voz,<br>
+Quemarle la tez de pies a cabeza<br>
+Esperando que vuelva a bailar.</p>
+
+<p class="verse">Vivo con ella.<br>
+Me escucha llorarle<br>
+A memorias profundas, inútiles<br>
+Y sin sentido.</p>
+
+<p class="verse">Canto y sollozo.<br>
+Encantado por su voz.<br>
+Y nunca tendré la fuerza <br>
+Para quitármela de encima…</p>
+
+<p class="verse">Que puta la música…</p>
+
+    `,
+  },
+  {
+    id: "XVII",
+    title: "XVII",
+    text: `<p class="verse"><strong>Un,</strong> dos, tres<br>
+Un, <strong>dos,</strong> tres<br>
+<strong>Un,</strong> dos</p>
+
+<p class="verse">Baila más pegaito nena<br>
+Que a esta distancia no me entiendes<br>
+Ritmo suave suda sensual<br>
+No es que vaya a parte</p>
+    `,
+  },
+  {
+    id: "XVIII",
+    title: "XVIII",
+    text: `<p class="verse">Steel your daughters’ heart,<br>
+Against artists and romantics.<br>
+Lest a silver-tongued devil,<br>
+Heart your daughters’ steal.</p>
+    `,
+  },
+  {
+    id: "XIX",
+    title: "XIX",
+    text: `<p class="verse">How must it feel,<br>
+To be a castaway,<br>
+Build your home,<br>
+Live from your island,<br>
+Accept your peace,<br>
+And spy a ship in the ocean?</p>
+
+<p class="verse">Fucking... swim.</p>
+
+<p class="verse">To the horizon if you make it.<br>
+But swim, you forsaken moron.</p>
+
+<p class="verse">****</p>
+
+<p class="verse">¿Cómo se siente ser náufrago,<br>
+Construir tu casa,<br>
+Vivir de tu isla,<br>
+Alcanzar la paz,<br>
+Y ver un barco en el mar?</p>
+
+<p class="verse">Nada coño.</p>
+
+<p class="verse">Hasta el horizonte si lo alcanzas.<br>
+Pero nada maldito idiota.</p>
+    `,
+  },
+  {
+    id: "XX",
+    title: "XX",
+    text: `<p class="verse">Así en ignorancia,<br>
+Quiero escribir un poema.<br> 
+Así sin sentido,<br>
+Quiero tener la razón. <br>
+Así inalcanzable,<br>
+Quiero perfección.<br>
+Así pesado,<br>
+Quiero ser ligero.</p>
+
+<p class="verse">Viene</p>
+
+<p class="verse">Y va</p>
+
+<p class="verse">Viene</p>
+
+<p class="verse">Y</p>
+
+<p class="verse">Va</p>
+
+<p class="verse">Duermo bien, pero no descanso.<br> 
+Estoy feliz, pero no satisfecho.</p>
+
+<p class="verse">Coño</p>
+
+<p class="verse">Pero bien. Porque estoy bien. <br>
+Y así sigo. </p>
+
+<p class="verse">Así fui,<br>
+Y así siempre voy.</p>
+    `,
+  },
+  {
+    id: "XXI",
+    title: "XXI",
+    text: `<p class="verse">Teach me how to fly<br>
+And I’ll be on my way.</p>
+    `,
+  },
+  {
+    id: "XXII",
+    title: "XXII",
+    text: `<p class="verse">Me inunda la curiosidad<br>
+Al haberme enseñado tanto<br>
+Ver cuanto te falta por entender.</p>
+    `,
+  },
+  {
+    id: "XXIII",
+    title: "XXIII",
+    text: `<p class="verse">Imagine the delight ,<br>
+Lonely souls finding company. </p>
+
+<p class="verse">Imagine the fright,<br>
+Admitting to a longing. </p>
+
+<p class="verse">Imagine how swell,<br>
+Feeling neither letting go. </p>
+
+<p class="verse">Imagine how nice,<br>
+Lonely lovers together…<br>
+passing time.</p>
+    `,
+  },
+  {
+    id: "XXIV",
+    title: "XXIV",
+    text: `<p class="verse">Hoy,</p>
+
+<p class="verse">Mi respiro al despertar<br>
+Capturó el celaje de cabellos amados.</p> 
+
+<p class="verse">Abrazando con ternura mi almohada,<br>
+Podría jurar, sostenía a mi amor. </p>
+
+<p class="verse">¿Habrá sido un sueño?<br>
+
+<p class="verse">De esos tan reales,<br>
+¿nos engañan nuestro imaginar?</p>
+
+<p class="verse">Mis manos lloraron buscándole<br>
+Entre sábanas hasta la esquina de la cama.<br>
+Tocaron mis ojos la memoria. </p>
+
+<p class="verse">Aquí estuvo. Anoche. <br>
+
+<p class="verse">Su corta memoria habrá olvidado<br>
+Llevarse la presencia de su aroma.<br>
+¿O lo habrá dejado a sabiendas,<br>
+Para que la recuerde?</p>
+
+<p class="verse">¿O volverme loco extrañándola?</p>
+
+<p class="verse">Cualquiera, y ambas;<br>
+Me hace feliz. </p>
+
+<p class="verse">Deseos de besarla. <br>
+Deseos de sostenerla. <br>
+Deseos de escucharla. </p>
+
+<p class="verse">Ya despierto,</p>
+
+<p class="verse">La sonrisa de mi alma en los labios.<br>
+Ya vendrá, y seguirá el sueño donde quedamos…</p>
+
+<p class="verse">… despiertos.</p>
+`,
+  },
+  {
+    id: "XXV",
+    title: "XXV",
+    text: `<p class="verse">Hoy quisiera ser poeta<br>
+Saber ponerte en palabras<br>
+Admirarte lujos de belleza<br>
+Y entender que eres raras</p>
+    `,
+  },
+];
