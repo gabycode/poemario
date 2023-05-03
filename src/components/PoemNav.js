af63ad2de02c1f6f5dea7153987508ae5422b26a
gabycode <86427446+gabycode@users.noreply.github.com>
2023-05-03 10:47:46 -0400

    change component name

---
+++ src/components/PoemNav.js
@@ -0,0 +1,18 @@
+const PoemNav = ({ poems, handlePoemSelect, selectedPoemIndex }) => {
+  return (
+    <ul>
+      {poems.map((poem, index) => (
+        <li
+          key={index}
+          onClick={() => handlePoemSelect(index)}
+          className={`poem-number ${
+            selectedPoemIndex === index ? "active" : ""
+          }`}>
+          {poem.id}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default PoemNav;
