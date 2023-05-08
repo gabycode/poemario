bca183df4b669b770cf078b0e4b7a7b567214d63
gabycode <86427446+gabycode@users.noreply.github.com>
2023-05-08 14:58:30 -0400

    add: poem author

--- src/components/Poem.js
+++ src/components/Poem.js
@@ -3,7 +3,10 @@ const Poem = ({ title, text, poemContainerRef, poemChanged }) => {
     <div
       className={`poem ${poemChanged ? "fadein" : ""}`}
       ref={poemContainerRef}>
-      <h2 className="poem-title">{title}</h2>
+      <div className="poem-top">
+        <h2 className="poem-author">Ricardo Dominguez</h2>
+        <h2 className="poem-title">{title}</h2>
+      </div>
       <div className="gradient-top"></div>
       <div
         className="poem-text"
